f109303f3c348d4c085bfce17d1f0a58b7553f1e
Devgimun <devgimun@naver.com>
2024-01-09 17:40:23 +0900

    Feat: 다이어리 조회/삭제 테스트 코드 추가 - 테스트 코드를 HTTP 메소드 별 매핑

--- api/src/test/java/org/diary/api/domain/diary/controller/DiaryApiControllerTest.java
+++ api/src/test/java/org/diary/api/domain/diary/controller/DiaryApiControllerTest.java
@@ -18,7 +18,7 @@ import org.springframework.util.Assert;
 
 public class DiaryApiControllerTest extends TestMvcConfig {
 
-    private static final String DIARY_URL = "/api/diary";
+    private static final String BASE_URL = "/api/diary";
     private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Autowired
@@ -31,20 +31,46 @@ public class DiaryApiControllerTest extends TestMvcConfig {
     private TokenBusiness tokenBusiness;
 
     @Test
+    void viewDiaryList() throws JsonProcessingException {
+
+        // given(값 설정)
+        UserCommon userCommon = new UserCommon(userRepository, tokenBusiness); // - User 정보
+        DiaryCommon diaryCommon = new DiaryCommon(diaryRepository, userCommon.user_id);
+
+        // when(실행)
+        String result = doGetWithToken(BASE_URL, userCommon.token);
+
+        // then(검증)
+        JsonNode resultJson = objectMapper.readTree(result);
+        Assert.notNull(resultJson.get("body").get("id"), "일기 조회 실패");
+    }
+
+    @Test
+    void viewDiary() throws JsonProcessingException {
+
+        // given(값 설정)
+        UserCommon userCommon = new UserCommon(userRepository, tokenBusiness); // - User 정보
+        DiaryCommon diaryCommon = new DiaryCommon(diaryRepository, userCommon.user_id);
+
+        // when(실행)
+        String result = doGetWithToken(BASE_URL + "/" +diaryCommon.diary_id, userCommon.token);
+
+        // then(검증)
+        JsonNode resultJson = objectMapper.readTree(result);
+        Assert.notNull(resultJson.get("body").get("id"), "일기 조회 실패");
+    }
+
+    @Test
     void addDiary() throws JsonProcessingException {
 
         // given(값 설정)
+        UserCommon userCommon = new UserCommon(userRepository, tokenBusiness); // - User 정보
+        DiaryRegisterRequest diaryRegisterRequest = DiaryFixture.anUserFixture().diaryRegisterRequest(); // - 다이어리 작성 값
 
-        // - User 정보
-        UserCommon userCommon = new UserCommon(userRepository, tokenBusiness);
+        // when(실행)
+        String result = doPostWithToken(BASE_URL, diaryRegisterRequest, userCommon.token);
 
-        // - 다이어리 작성 값
-        DiaryRegisterRequest diaryRegisterRequest = DiaryFixture.anUserFixture().diaryRegisterRequest();
-
-        //when(실행)
-        String result = doPostWithToken(DIARY_URL + "/register", diaryRegisterRequest, userCommon.token);
-
-        //then(검증)
+        // then(검증)
         JsonNode resultJson = objectMapper.readTree(result);
         Assert.notNull(resultJson.get("body").get("id"), "일기 추가 실패");
     }
@@ -52,24 +78,33 @@ public class DiaryApiControllerTest extends TestMvcConfig {
     @Test
     void updateDiary() throws JsonProcessingException {
 
-        //given(값 설정)
+        // given(값 설정)
+        UserCommon userCommon = new UserCommon(userRepository, tokenBusiness);  // - 유저 생성
+        DiaryCommon diaryCommon = new DiaryCommon(diaryRepository, userCommon.user_id); // - 다이어리 생성
+        Long diary_id = diaryCommon.diary_id; // - 다이어리 정보
+        DiaryUpdateRequest diaryUpdateRequest = DiaryFixture.anUserFixture().diaryUpdateRequest(); // - 수정될 데이터
 
-        // - 유지 정보
-        UserCommon userCommon = new UserCommon(userRepository, tokenBusiness);
-        DiaryCommon diaryCommon = new DiaryCommon(diaryRepository, userCommon.user_id);
+        // when(실행)
+        String diaryUpdateResultJson = doPatchWithToken(BASE_URL + "/" + diary_id, diaryUpdateRequest, userCommon.token);
 
-        // - 다이어리 추가
-        Long diary_id = diaryCommon.diary_id;
-
-        // - 다이어리 수정 값
-        DiaryUpdateRequest diaryUpdateRequest = DiaryFixture.anUserFixture().diaryUpdateRequest();
-
-
-        //when(실행)
-        String diaryUpdateResultJson = doPostWithToken(DIARY_URL + "/update/" + diary_id, diaryUpdateRequest, userCommon.token);
-
-        //then(검증)
+        // then(검증)
         JsonNode updateResultJson = objectMapper.readTree(diaryUpdateResultJson);
         Assert.notNull(updateResultJson.get("body").get("id"), "일기 수정 실패");
+    }
+
+    @Test
+    void deleteDiary() throws JsonProcessingException {
+
+        // given(값 설정)
+        UserCommon userCommon = new UserCommon(userRepository, tokenBusiness);  // - 유저 생성
+        DiaryCommon diaryCommon = new DiaryCommon(diaryRepository, userCommon.user_id); // - 다이어리 생성
+        Long diary_id = diaryCommon.diary_id; // - 다이어리 정보
+
+        // when(실행)
+        String diaryUpdateResultJson = doDeleteWithToken(BASE_URL + "/" + diary_id, userCommon.token);
+
+        // then(검증)
+        JsonNode updateResultJson = objectMapper.readTree(diaryUpdateResultJson);
+        Assert.notNull(updateResultJson.get("body").get("id"), "일기 삭제 실패");
     }
 }
